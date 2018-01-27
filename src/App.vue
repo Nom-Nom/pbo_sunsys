6a359038b88bf583b377fff5430b23e7b13cb280
Nui <AngryNui@users.noreply.github.com>
2018-01-27 12:17:06 +0100

    CSS für Planeten

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,79 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<b-container>
+    <b-row class=test>
+        <b-col>
+          <b-list-group>
+            <b-list-group-item>Sonnensystem:</b-list-group-item>
+            <b-list-group-item>Anzahl der Planeten</b-list-group-item>
+            <b-list-group-item>
+              Sonnensystem wechseln?
+              <b-button>zurück</b-button>
+              <b-button>vor</b-button>
+            </b-list-group-item>
+        </b-col>
+        <b-col>
+          <div class=planetgroup>
+            <div class=planet style="background-image:url();z-index=1;">
+              </div>
+              <div class=planet style="background-image:url();z-index=2;">
+              </div>
+              <div class=planet style="background-image:url();z-index=3;background-size:500px;background-position:100px 300px">
+              </div>
+              <div class=planetAtmos style="">
+              </div>
+            </div>
+        </b-col>
+        <b-col>3 of 4</b-col>
+        <b-col>4 of 4</b-col>
+    </b-row>
+</b-container>
 </template>
-
+235
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Hi :>"
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-
-h1, h2 {
-  font-weight: normal;
+.planetgroup {
+  position: relative;
+  top: 75px;
+  left: 75px;
+  width: 150px;
+  height: 150px;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+.planet {
+  border-radius: 50%;
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 150px;
+  height: 150px;
+  background-size: 150px;
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.planetAtmos {
+  border-radius: 50%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 160px;
+  height: 160px;
+  background-color: rgba(255, 0, 0, 0.2);
+  filter: blur(3px);
+  mix-blend-mode:multiply;
 }
 </style>
