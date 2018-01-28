455dabd12a48a5dc3e05b9b7042a7e3a6f342e3e
Nui <AngryNui@users.noreply.github.com>
2018-01-28 01:42:06 +0100

    landingpage realisiert

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-<b-container>
+<b-container-fluid>
+  <header>
+    <img class="openerPlanets" src="./assets/openerPlanets.png">
+    <img class="openerSchrift" src="./assets/openerSchrift.png">
+    <span class="author">von Hannes Liehr und Anna Krauß</span>
+  </header>
     <b-row class=test>
         <b-col>
           <div>{{atmosphaereColor(1)}}</div>
         </b-col>
         <b-col>
         <div class=planetgroup>
+          <b-button v-on:click="id+=1">Hi</b-button>
           <div class=planet style="background-image:url();z-index=1;">
             </div>
             <div class=planet style="background-image:url();z-index=2;">
             </div>
-            <div class=planet style="background-image:url();z-index=3;background-size:500px;background-position:100px 300px">
+            <div class=planet style="background-image:url();z-index=3;background-size:500px;background-position:100px 300px" v-bind:style="atomosphaereColor(id)">
             </div>
             <div class=planetAtmos style="">
             </div>
@@ -18,9 +24,8 @@
         </b-col>
         <b-col>2/3</b-col>
     </b-row>
-</b-container>
+</b-container-fluid>
 </template>
-235
 <script>
 import json from './process.json';
 export default {
@@ -28,7 +33,8 @@ export default {
   data() {
     return {
       msg: "Hi :>",
-      json: json
+      json: json,
+      id: 1
     };
   },
   methods: {
@@ -52,6 +58,24 @@ export default {
       }
     return proz;
     },
+    
+    // Nui hat quatsch gemacht 
+    atomosphaereColor: function(value) {
+      if (value == 1) {
+        return "background-color:rgba(255," + 255 + ",0, 0.2)";
+      }
+      if (value == 2) {
+        return "background-color:rgba(255," + 0 + ",0, 0.2)";
+      }
+      if (value == 3) {
+        return "background-color:rgba(255," + 60 + ",0, 0.2)";
+      }
+      if (value == 4) {
+        return "background-color:rgba(255," + 170 + ",0, 0.2)";
+      }
+    },
+    // Bis hier hin :>
+
     atmosphaereColor: function(childnr){
       var sh;
       var chnr=childnr;
@@ -135,8 +159,59 @@ export default {
   left: 0px;
   width: 160px;
   height: 160px;
-  background-color: rgba(255, 0, 0, 0.2);
   filter: blur(3px);
-  mix-blend-mode:multiply;
+  mix-blend-mode: multiply;
+  background-color: rgba(255, 0, 0, 0.2);
+}
+header{
+  background: radial-gradient(
+  circle farthest-side at 50% 0px,
+    #330d48 0%,
+    #061840 64%,
+    #05143a 100%
+  );
+  height: 100vh;
+  text-align: center;
+}
+.openerPlanets{
+  position: absolute;
+  display:block;
+  margin-left: auto;
+  margin-right: auto;
+  left: 40px;
+  right: 0;
+  padding-top:15vh;
+  max-width: 760px;
+  z-index: 1;
+}
+.openerSchrift{
+  position: absolute;
+  display:block;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  max-width: 760px;
+  z-index: 2;
+}
+.container-fluid{
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+  margin-left: 0px !important;
+  margin-right: 0px !important;
+}
+.author{
+  top: 95vh;
+  position: absolute;
+  display:block;
+  z-index: 3;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  color: #3462ac;
+}
+body{
+  overflow-x: hidden;
 }
 </style>
