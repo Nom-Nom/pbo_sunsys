58135da9d52fabe61034090d7479156f72ac2a79
Nui <AngryNui@users.noreply.github.com>
2018-01-30 17:06:20 +0100

    CSS-Anpassungen

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,17 @@
         </b-col>
         <b-col>
         <div class=planetgroup>
+          <div class="planet atmos hgAtmos">
+          </div>
           <div class="planet gt" style="background-size:500%;" v-bind:style="grundTexturPosition(id)" >
           </div>
           <div class="planet gtFarbe" style="mix-blend-mode:overlay;" v-bind:style="gtFarbe(id)">
           </div>
           <div class="planet wasser" v-bind:style="wasserZoom(id)+''+wasserPosition(id)">
           </div>
-          <div class="planet verlauf">
+          <div class="planet verlauf" v-bind:style="atmosphaereColor(id/*,true*/)">
           </div>
-          <div class="planet atmos" v-bind:style="atmosphaereColor(id)">
+          <div class="planet atmos" v-bind:style="atmosphaereColor(id/*,false*/)">
           </div>
         </div>
         </b-col>
@@ -61,7 +63,7 @@ export default {
       }
       return proz;
     },
-    atmosphaereColor: function(childnr) {
+    atmosphaereColor: function(childnr/*, hg*/) {
       var sh;
       var chnr = childnr;
       var rgba;
@@ -72,102 +74,148 @@ export default {
       sh = sh % 3;
       switch (chnr) {
         case 0:
-          rgba = "background-color:rgba(255,"+light+","+light+","+trans+");";
+          rgba =
+            "background-color:rgba(255," +
+            light +
+            "," +
+            light +
+            "," +
+            trans +
+            ");";
           break;
         case 1:
-          rgba = "background-color:rgba(255,255,"+light+","+trans+");";
+          rgba = "background-color:rgba(255,255," + light + "," + trans + ");";
           break;
         case 2:
-          rgba = "background-color:rgba(255,255,255,"+trans+");";
+          rgba = "background-color:rgba(255,255,255," + trans + ");";
           break;
       }
       if (chnr == 1) {
         switch (sh) {
           case 0:
-            rgba = "background-color:rgba("+light+","+light+",255,"+trans+");";
+            rgba =
+              "background-color:rgba(" +
+              light +
+              "," +
+              light +
+              ",255," +
+              trans +
+              ");";
             break;
           case 1:
-            rgba = "background-color:rgba(255,"+light+","+light+","+trans+");";
+            rgba =
+              "background-color:rgba(255," +
+              light +
+              "," +
+              light +
+              "," +
+              trans +
+              ");";
             break;
           case 2:
-            rgba = "background-color:rgba("+light+",255,"+light+","+trans+");";
+            rgba =
+              "background-color:rgba(" +
+              light +
+              ",255," +
+              light +
+              "," +
+              trans +
+              ");";
             break;
         }
       }
       if (chnr == 2) {
         switch (sh) {
           case 0:
-            rgba = "background-color:rgba(255,"+light+",255,"+trans+");";
+            rgba =
+              "background-color:rgba(255," + light + ",255," + trans + ");";
             break;
           case 1:
-            rgba = "background-color:rgba(255,255,"+light+","+trans+");";
+            rgba =
+              "background-color:rgba(255,255," + light + "," + trans + ");";
             break;
           case 2:
-            rgba = "background-color:rgba("+light+",255,255,"+trans+");";
+            rgba =
+              "background-color:rgba(" + light + ",255,255," + trans + ");";
             break;
         }
       }
-      return rgba;
+      // if (hg==true){
+      //   rgba=rgba.match(/([\d{}+]*,){3}/g)
+      //   rgba=rgba.split(",", 3);
+      //   console.log(rgba+"Hallo")
+      // }
+       return rgba;
     },
-    wasserPosition: function (childnr) {
-      var count=0;
-      var proz1=0; var proz2=0;
+    wasserPosition: function(childnr) {
+      var count = 0;
+      var proz1 = 0;
+      var proz2 = 0;
       var wasserpos;
-      var str=json.process.childs[childnr].description;
-      for(var i=0; i<str.length; i++){
-        count+=str.charCodeAt(i);
+      var str = json.process.childs[childnr].description;
+      for (var i = 0; i < str.length; i++) {
+        count += str.charCodeAt(i);
         //console.log(count);
       }
-      proz1=count % 100;
+      proz1 = count % 100;
       //console.log(proz1);
-      proz2=(proz1 % 51) * 2;
+      proz2 = (proz1 % 51) * 2;
       //console.log(proz2);
-      wasserpos="background-position: " + proz1 + "% " + proz2 + "%;";
+      wasserpos = "background-position: " + proz1 + "% " + proz2 + "%;";
 
       return wasserpos;
     },
-    grundTexturPosition: function (childnr) {
-      var count=0;
-      var proz1=0; var proz2=0;
+    grundTexturPosition: function(childnr) {
+      var count = 0;
+      var proz1 = 0;
+      var proz2 = 0;
       var texturpos;
-      var str=json.process.childs[childnr].start;
-      for(var i=0; i<str.length;i++){
-        count+=str.charCodeAt(i);
+      var str = json.process.childs[childnr].start;
+      for (var i = 0; i < str.length; i++) {
+        count += str.charCodeAt(i);
         //console.log(count);
       }
-      proz1=count % 100;
+      proz1 = count % 100;
       //console.log(proz1);
-      proz2=(proz1 % 51) * 2;
+      proz2 = (proz1 % 51) * 2;
       //console.log(proz2);
-      texturpos="background-position: " + proz1 + "% " + proz2 + "%;";
+      texturpos = "background-position: " + proz1 + "% " + proz2 + "%;";
 
       return texturpos;
     },
-     gtFarbe: function (childnr) {
-      var count1=1;
-      var count2=1;
-      var count3=1;
+    gtFarbe: function(childnr) {
+      var count1 = 1;
+      var count2 = 1;
+      var count3 = 1;
       var rgb;
-      var proz1=0; var proz2=0;
+      var proz1 = 0;
+      var proz2 = 0;
       var gtFarbe;
-      var split=json.process.childs[childnr].name.length/3;
-      split=parseInt(split);
-      var str1=json.process.childs[childnr].name.substr(0,split-1);
-      var str2=json.process.childs[childnr].name.substr(split-1,split*2-1);
-      var str3=json.process.childs[childnr].name.substr((split*2)-1,json.process.childs[childnr].name.length);
-      for(var i=0; i< str1.length; i++){
-        if (str1.charCodeAt(i)!=0) count1+=str1.charCodeAt(i);
+      var split = json.process.childs[childnr].name.length / 3;
+      split = parseInt(split);
+      var str1 = json.process.childs[childnr].name.substr(0, split - 1);
+      var str2 = json.process.childs[childnr].name.substr(
+        split - 1,
+        split * 2 - 1
+      );
+      var str3 = json.process.childs[childnr].name.substr(
+        split * 2 - 1,
+        json.process.childs[childnr].name.length
+      );
+      for (var i = 0; i < str1.length; i++) {
+        if (str1.charCodeAt(i) != 0) count1 += str1.charCodeAt(i);
       }
-      for(var i=0; i< str2.length; i++){
-        if (str2.charCodeAt(i)!=0)count2+=str2.charCodeAt(i);
+      for (var i = 0; i < str2.length; i++) {
+        if (str2.charCodeAt(i) != 0) count2 += str2.charCodeAt(i);
       }
-      for(var i=0; i< str3.length; i++){
-        if (str3.charCodeAt(i)!=0)count3+=str3.charCodeAt(i);
+      for (var i = 0; i < str3.length; i++) {
+        if (str3.charCodeAt(i) != 0) count3 += str3.charCodeAt(i);
       }
-      count1=count1 % 256;
-      count2=count2 % 256;
-      count3=count3 % 256;
-      rgb="background-color:rgb(" + count1 + "," + count2 + "," + count3 + ");";
+      count1 = count1 % 256;
+      count2 = count2 % 256;
+      count3 = count3 % 256;
+      rgb =
+        "background-color:rgb(" + count1 + "," + count2 + "," + count3 + ");";
       return rgb;
     }
   }
@@ -184,45 +232,58 @@ export default {
   margin-top: 60px;
 }
 .planetgroup {
-  top:50%;
+  margin-top: 10% !important;
+  top: 50%;
   position: relative;
-  margin:auto;
+  margin: auto;
   width: 170px;
   height: 170px;
-  transform:translateY(-50%)
+  transform: translateY(-50%);
 }
 .planet {
   border-radius: 50%;
   position: absolute;
-  top: 10px;
-  left: 10px;
-  width: 150px;
-  height: 150px;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
   background-size: 150px;
 }
 .atmos {
-  top: 0px;
-  left: 0px;
-  width: 170px;
-  height: 170px;
-  mix-blend-mode: screen;
+  top: -5%;
+  left: -5%;
+  width: 110%;
+  height: 110%;
+  mix-blend-mode: multiply;
   z-index: 4;
 }
-.verlauf{
-  mix-blend-mode: multiply;
+.hgAtmos {
   background: radial-gradient(
-  circle farthest-side at 0px 0px,
-  rgba(255, 255, 255, 0) 0%,
-  rgba(61, 61, 61, 1)100%
+    circle farthest-side at 0px 0px,
+    rgba(255, 255, 255, 0.1) 0%,
+    rgba(255, 255, 255, 0.01)100%
+  );
+  top: -15%;
+  left: -15%;
+  width: 130%;
+  height: 130%;
+  z-index: 1 !important;
+}
+.verlauf {
+  mix-blend-mode: normal;
+  background: radial-gradient(
+    circle farthest-side at 0px 0px,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(20, 0, 0, 1)100%
   );
   z-index: 3;
 }
-.gt{
-  background-image:url(/src/assets/grundTextur.svg);
-  z-index:1;
+.gt {
+  background-image: url(/src/assets/grundTextur.svg);
+  z-index: 1;
 }
-.gtFarbe{
-  z-index:1;
+.gtFarbe {
+  z-index: 1;
 }
 .wasser {
   background-image: url(/src/assets/wasserTextur.svg);
@@ -247,7 +308,7 @@ header {
   right: 0;
   padding-top: 15vh;
   max-width: 760px;
-  max-height:100vh;
+  max-height: 100vh;
   z-index: 1;
 }
 .openerSchrift {
@@ -278,15 +339,14 @@ header {
   right: 0;
   color: #3462ac;
 }
-.weiter{
-
+.weiter {
 }
-.zurueck{
-  margin-left:100%;
-  transform:translateX(-100%);
+.zurueck {
+  margin-left: 100%;
+  transform: translateX(-100%);
 }
 body {
   overflow-x: hidden;
-  background-color: #05143a;
+  background-color: rgb(12, 11, 16);
 }
 </style>
